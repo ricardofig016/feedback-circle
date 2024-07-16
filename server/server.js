3ed67e3f290214e8adde899bba074803f93bf319
Ricardo Castro <RicardoCastro@criticalmanufacturing.com>
2024-07-16 12:05:20 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3", "user4"] });
+});
+
+app.listen(5000, () => {
+  console.log("Server listening at http://localhost:5000");
+});
