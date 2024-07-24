a17ac746a855ce1780572acfe8158497f31d23e3
Ricardo Castro <RicardoCastro@criticalmanufacturing.com>
2024-07-24 11:55:47 +0100

    Formatting

--- utilities/icons.js
+++ utilities/icons.js
@@ -1,16 +1,14 @@
-'use strict';
-
+"use strict";
 
 const Icons = {
-    checkmark:              '<span class="fa-icon"><i class="fa fa-check" aria-hidden="true"></i></span>',
-    green_checkmark_full:   '<span class="fa-icon fa-icon-green"><i class="fa fa-check-circle" aria-hidden="true"></i></span>',
-    red_exclamation_mark:   '<span class="fa-icon fa-icon-red"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>',
-    green_plug:             '<span class="fa-icon"><i class=" fa fa-solid fa-plug" aria-hidden="true"></i></span>',
-    toast_success:          '<i class="fa fa-check-circle" aria-hidden="true" style="color: #50B450"></i>',
-    toast_error:            '<i class="fa fa-exclamation-circle" aria-hidden="true" style="color: #EA4232"></i>',
-    toast_warning:          '<i class="fa fa-exclamation-triangle" aria-hidden="true" style="color: #f69414"></i>',
-    toast_info:             '<i class="fa fa-info-circle" aria-hidden="true" style="color: #3498db"></i>' 
+  checkmark: '<span class="fa-icon"><i class="fa fa-check" aria-hidden="true"></i></span>',
+  green_checkmark_full: '<span class="fa-icon fa-icon-green"><i class="fa fa-check-circle" aria-hidden="true"></i></span>',
+  red_exclamation_mark: '<span class="fa-icon fa-icon-red"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>',
+  green_plug: '<span class="fa-icon"><i class=" fa fa-solid fa-plug" aria-hidden="true"></i></span>',
+  toast_success: '<i class="fa fa-check-circle" aria-hidden="true" style="color: #50B450"></i>',
+  toast_error: '<i class="fa fa-exclamation-circle" aria-hidden="true" style="color: #EA4232"></i>',
+  toast_warning: '<i class="fa fa-exclamation-triangle" aria-hidden="true" style="color: #f69414"></i>',
+  toast_info: '<i class="fa fa-info-circle" aria-hidden="true" style="color: #3498db"></i>',
 };
-
 
 export default Icons;
