b45aca008c288137d50bf51b0f7a6bfc85b91496
Ricardo Castro <RicardoCastro@criticalmanufacturing.com>
2024-07-25 14:23:57 +0100

    Formatting

--- ui/modules/progress-indicator/progress-indicator.js
+++ ui/modules/progress-indicator/progress-indicator.js
@@ -1,41 +1,38 @@
-'use strict';
+"use strict";
 
 export default class ProgressIndicator {
-    static #progressIndicatorElement;
-    static #progressIndicatorLoadingElement;
-    static #instance; // Singleton instance
+  static #progressIndicatorElement;
+  static #progressIndicatorLoadingElement;
+  static #instance; // Singleton instance
 
-
-    constructor() {
-        if (ProgressIndicator.#instance instanceof ProgressIndicator) {
-            return ProgressIndicator.#instance;
-        }
-
-        ProgressIndicator.#progressIndicatorElement = document.getElementById('progress-indicator');
-        ProgressIndicator.#progressIndicatorLoadingElement = document.getElementById('progress-indicator-loading');
-
-        // Singleton logic
-        ProgressIndicator.#instance = this;
-        return ProgressIndicator.#instance;
+  constructor() {
+    if (ProgressIndicator.#instance instanceof ProgressIndicator) {
+      return ProgressIndicator.#instance;
     }
 
+    ProgressIndicator.#progressIndicatorElement = document.getElementById("progress-indicator");
+    ProgressIndicator.#progressIndicatorLoadingElement = document.getElementById("progress-indicator-loading");
 
-    show() {
-        if (ProgressIndicator.#progressIndicatorElement) {
-            ProgressIndicator.#progressIndicatorElement.classList.add('active');
-        }
-        if (ProgressIndicator.#progressIndicatorLoadingElement) {
-            ProgressIndicator.#progressIndicatorLoadingElement.classList.add('active');
-        }
+    // Singleton logic
+    ProgressIndicator.#instance = this;
+    return ProgressIndicator.#instance;
+  }
+
+  show() {
+    if (ProgressIndicator.#progressIndicatorElement) {
+      ProgressIndicator.#progressIndicatorElement.classList.add("active");
     }
-
-
-    hide() {
-        if (ProgressIndicator.#progressIndicatorElement) {
-            ProgressIndicator.#progressIndicatorElement.classList.remove('active');
-        }
-        if (ProgressIndicator.#progressIndicatorLoadingElement) {
-            ProgressIndicator.#progressIndicatorLoadingElement.classList.remove('active');
-        }
+    if (ProgressIndicator.#progressIndicatorLoadingElement) {
+      ProgressIndicator.#progressIndicatorLoadingElement.classList.add("active");
     }
-};
+  }
+
+  hide() {
+    if (ProgressIndicator.#progressIndicatorElement) {
+      ProgressIndicator.#progressIndicatorElement.classList.remove("active");
+    }
+    if (ProgressIndicator.#progressIndicatorLoadingElement) {
+      ProgressIndicator.#progressIndicatorLoadingElement.classList.remove("active");
+    }
+  }
+}
