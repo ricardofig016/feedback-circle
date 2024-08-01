c377d98bde3c38cce9c628e6180bac538f2242f2
Ricardo Castro <RicardoCastro@criticalmanufacturing.com>
2024-08-01 17:59:58 +0100

    Add more mock data for feedbacks database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,3 +1,4 @@
+--@block
 -- remove database
 DROP DATABASE IF EXISTS performance_feedback_circle;
 --
@@ -55,8 +56,15 @@ CREATE TABLE IF NOT EXISTS feedbacks(
     FOREIGN KEY (sender_id) REFERENCES users(user_id),
     FOREIGN KEY (receiver_id) REFERENCES users(user_id)
 );
---
---
+--@block
+-- show tables
+SELECT *
+FROM users;
+SELECT *
+FROM user_circle;
+SELECT *
+FROM feedbacks;
+--@block
 -- insert dummy data
 INSERT INTO users (
         name,
@@ -170,14 +178,136 @@ VALUES (
         "private",
         "both",
         "this is a feedback sent from vasco to duarte"
+    ),
+    (
+        1,
+        1,
+        "ricardo to ricardo",
+        "feedback from ricardo to ricardo",
+        "2001-01-01 00:00:00",
+        "general",
+        "negative",
+        "anonymous",
+        "appraiser",
+        "this is a feedback sent from ricardo to ricardo"
+    ),
+    (
+        2,
+        2,
+        "sonia to sonia",
+        "feedback from sonia to sonia aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla",
+        "2002-02-02 00:00:00",
+        "execution-and-delivery",
+        "positive",
+        "private",
+        "both",
+        "this is a feedback sent from sonia to sonia aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla"
+    ),
+    (
+        3,
+        3,
+        "duarte to duarte",
+        "feedback from duarte to duarte",
+        "2003-03-03 00:00:00",
+        "innovation",
+        "negative",
+        "public",
+        "appraiser",
+        "this is a feedback sent from duarte to duarte"
+    ),
+    (
+        4,
+        4,
+        "vasco to vasco",
+        "feedback from vasco to vasco",
+        "2004-04-04 00:00:00",
+        "agility",
+        "positive",
+        "anonymous",
+        "both",
+        "this is a feedback sent from vasco to vasco"
+    ),
+    (
+        1,
+        3,
+        "ricardo to duarte",
+        "feedback from ricardo to duarte",
+        "2005-05-05 00:00:00",
+        "commitment",
+        "negative",
+        "private",
+        "appraiser",
+        "this is a feedback sent from ricardo to duarte"
+    ),
+    (
+        2,
+        3,
+        "sonia to duarte",
+        "feedback from sonia to duarte",
+        "2006-06-06 00:00:00",
+        "communication",
+        "positive",
+        "public",
+        "both",
+        "this is a feedback sent from sonia to duarte"
+    ),
+    (
+        3,
+        3,
+        "duarte to duarte",
+        "feedback from duarte to duarte aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla",
+        "2007-07-20 00:00:00",
+        "customer-orientation",
+        "negative",
+        "anonymous",
+        "appraiser",
+        "this is a feedback sent from duarte to duarte aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla"
+    ),
+    (
+        4,
+        3,
+        "vasco to duarte",
+        "feedback from vasco to duarte aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla",
+        "2008-08-21 00:00:00",
+        "general",
+        "positive",
+        "private",
+        "both",
+        "this is a feedback sent from vasco to duarte aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla"
+    ),
+    (
+        1,
+        4,
+        "ricardo to vasco",
+        "feedback from ricardo to vasco",
+        "2009-09-22 00:00:00",
+        "execution-and-delivery",
+        "negative",
+        "public",
+        "appraiser",
+        "this is a feedback sent from ricardo to vasco"
+    ),
+    (
+        4,
+        1,
+        "vasco to ricardo",
+        "feedback from vasco to ricardo aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla",
+        "2010-10-29 00:00:00",
+        "innovation",
+        "positive",
+        "anonymous",
+        "both",
+        "this is a feedback sent from vasco to ricardo aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla aslkdjhfla"
+    ),
+    (
+        2,
+        1,
+        "sonia to ricardo",
+        "feedback from sonia to ricardo",
+        "2011-11-30 00:00:00",
+        "agility",
+        "negative",
+        "private",
+        "appraiser",
+        "this is a feedback sent from sonia to ricardo"
     );
---
---
---@block
--- show tables
-SELECT *
-FROM users;
-SELECT *
-FROM user_circle;
-SELECT *
-FROM feedbacks;
